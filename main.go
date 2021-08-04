1f81faae5cb3c2656feb5986121c4abc625486d0
Afonso Cabral <afns.cbrl@gmail.com>
2021-08-04 14:11:50 -0300

    Https redirect14

--- main.go
+++ main.go
@@ -8,9 +8,17 @@ import (
 	"os"
 )
 
-// func redirectToHttps(w http.ResponseWriter, r *http.Request) {
-// 	http.Redirect(w, r, "https://"+r.Host+r.URL.Path, http.StatusMovedPermanently)
-// }
+func redirectToHttps(w http.ResponseWriter, r *http.Request) {
+	// remove/add not default ports from req.Host
+	target := "https://" + r.Host + r.URL.Path
+	if len(r.URL.RawQuery) > 0 {
+		target += "?" + r.URL.RawQuery
+	}
+	log.Printf("redirect to: %s", target)
+	http.Redirect(w, r, target,
+		// see comments below and consider the codes 308, 302, or 301
+		http.StatusTemporaryRedirect)
+}
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	var tmpl = template.Must(template.ParseGlob("index.html"))
@@ -25,6 +33,10 @@ func main() {
 	}
 
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
-	http.HandleFunc("/", Index)
-	http.ListenAndServe(":"+port, nil)
+	// redirect every http request to https
+	go http.ListenAndServe(":"+port, http.HandlerFunc(redirectToHttps))
+	// serve index (and anything else) as https
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Index)
+	http.ListenAndServeTLS(":443", "cert.pem", "key.pem", mux)
 }
