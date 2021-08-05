d38596f58f1f4b5a8af89b7f8dde6f517e97ddfd
Afonso Cabral <afns.cbrl@gmail.com>
2021-08-05 11:35:32 -0300

    redirect saga3.8

--- main.go
+++ main.go
@@ -8,28 +8,28 @@ import (
 	"os"
 )
 
-func redirect(w http.ResponseWriter, req *http.Request) {
-	target := "https://" + req.Host + req.URL.Path
-	if len(req.URL.RawPath) > 0 {
-		target += "/" + req.URL.RawPath
-	}
-	log.Printf("redirect to: %s", target)
-	http.Redirect(w, req, target,
-		// see comments below and consider the codes 308, 302, or 301
-		http.StatusTemporaryRedirect)
-}
+// func redirect(w http.ResponseWriter, req *http.Request) {
+// 	target := "https://" + req.Host + req.URL.Path
+// 	if len(req.URL.Raw) > 0 {
+// 		target += "/" + req.URL.RawPath
+// 	}
+// 	log.Printf("redirect to: %s", target)
+// 	http.Redirect(w, req, target,
+// 		// see comments below and consider the codes 308, 302, or 301
+// 		http.StatusTemporaryRedirect)
+// }
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	var tmpl = template.Must(template.ParseGlob("index.html"))
 	data := scraps.Scraping()
 	tmpl.ExecuteTemplate(w, "Index", data)
 
-	if r.URL.Path != "/" {
-		log.Printf("404: %s", r.URL.String())
-		http.NotFound(w, r)
-		return
-	}
-	http.ServeFile(w, r, "index.html")
+	// if r.URL.Path != "/" {
+	// 	log.Printf("404: %s", r.URL.String())
+	// 	http.NotFound(w, r)
+	// 	return
+	// }
+	// http.ServeFile(w, r, "index.html")
 }
 
 func main() {
@@ -38,7 +38,7 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	// http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.HandleFunc("/", Index)
-	http.ListenAndServe(":"+port, http.HandlerFunc(redirect))
+	http.ListenAndServe(":"+port, nil)
 }
